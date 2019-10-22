78e6e8ab64a76e475916ac2d05a918ec0ff2ffb3
Mateusz Nowakowski <mateusz.nowakowski.programista@gmail.com>
2019-10-23 00:19:43 +0200

    Divide KsiazkaAdresowa to smaler class

---
+++ MetodyPomocniczne.cpp
@@ -0,0 +1,9 @@
+#include "MetodyPomocniczne.h"
+
+string MetodyPomocniczne::konwersjaIntNaString(int liczba)
+{
+    ostringstream ss;
+    ss << liczba;
+    string str = ss.str();
+    return str;
+}
